d8225b03cd936b93e921a0f886cc4c0e6f72946d
Hakim Hauston <hakim.hauston@storms.com>
2021-08-20 17:18:47 +0800

    code: always call setState

--- 004-flutter-basics/demo_start/lib/home.dart
+++ 004-flutter-basics/demo_start/lib/home.dart
@@ -8,19 +8,40 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<String> _pageData = [];
+  List<String> _pageData;
+
+  bool get _fetchingData => _pageData == null;
 
   // Return a list of data after 1 second to emulate network request
-  Future<List<String>> _getListData() async {
-    await Future.delayed(Duration(seconds: 1));
+  Future<List<String>> _getListData(
+      {bool hasError = false, bool hasData = true}) async {
+    await Future.delayed(Duration(seconds: 2));
+
+    if (hasError) {
+      return Future.error(
+          'An error occurred while fetching the data. Please try again later');
+    }
+
+    if (!hasData) {
+      return [];
+    }
+
     return List<String>.generate(10, (index) => '$index title');
   }
 
   @override
   void initState() {
-    _getListData().then((data) {
+    _getListData(hasError: true).then((data) {
       setState(() {
+        if (data.length == 0) {
+          data.add(
+              'No data found for your account. Add something and check back');
+        }
         _pageData = data;
+      });
+    }).catchError((error) {
+      setState(() {
+        _pageData = [error];
       });
     });
     super.initState();
@@ -29,16 +50,27 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: _pageData.length,
-          itemBuilder: (buildContext, index) {
-            return Container(
-              margin: EdgeInsets.all(5.0),
-              height: 50.0,
-              color: Colors.grey[700],
-              child: Text(_pageData[index]),
-            );
-          }),
+        body: _fetchingData
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: _pageData.length,
+                itemBuilder: (buildContext, index) => _getListItemUi(index),
+              ));
+  }
+
+  Widget _getListItemUi(int index) {
+    return Container(
+      margin: EdgeInsets.all(5.0),
+      height: 50.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5.0),
+        color: Colors.grey[600],
+      ),
+      child: Center(
+          child: Text(
+        _pageData[index],
+        style: TextStyle(color: Colors.white),
+      )),
     );
   }
 }
