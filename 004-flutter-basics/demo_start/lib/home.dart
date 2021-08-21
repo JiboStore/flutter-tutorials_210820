3dd6a8f9c6bf2150b5255a94e09d4774e2b1a1f5
Hakim Hauston <hakim.hauston@storms.com>
2021-08-21 14:28:07 +0800

    code: use FutureBuilder, no need to call setState

--- 004-flutter-basics/demo_start/lib/home.dart
+++ 004-flutter-basics/demo_start/lib/home.dart
@@ -1,12 +1,89 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
+  /*
   @override
   State<StatefulWidget> createState() {
     return _HomeState();
   }
+  */
+
+  // Return a list of data after 1 second to emulate network request
+  Future<List<String>> _getListData(
+      {bool hasError = false, bool hasData = true}) async {
+    await Future.delayed(Duration(seconds: 2));
+
+    if (hasError) {
+      return Future.error(
+          'An error occurred while fetching the data. Please try again later');
+    }
+
+    if (!hasData) {
+      return [];
+    }
+
+    return List<String>.generate(10, (index) => '$index title');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[900],
+        body: FutureBuilder(
+          future: _getListData(hasError: true, hasData: false),
+          builder: (buildContext, snapshot) {
+            // error ui
+            if (snapshot.hasError) {
+              return _getInformationMessage(snapshot.error);
+            }
+            // Busy fetching data
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            var listItems = snapshot.data;
+
+            // when empty data is returned
+            if (listItems.length == 0) {
+              return _getInformationMessage('No data found for your account');
+            }
+
+            // build list if we have data
+            return ListView.builder(
+                itemCount: listItems.length,
+                itemBuilder: (buildContext, index) {
+                  return _getListItemUi(index, listItems);
+                });
+          },
+        ));
+  }
+
+  Widget _getListItemUi(int index, List<String> listItems) {
+    return Container(
+      margin: EdgeInsets.all(5.0),
+      height: 50.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5.0),
+        color: Colors.grey[600],
+      ),
+      child: Center(
+          child: Text(
+        listItems[index],
+        style: TextStyle(color: Colors.white),
+      )),
+    );
+  }
+
+  Widget _getInformationMessage(String message) {
+    return Center(
+        child: Text(message,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontWeight: FontWeight.w900, color: Colors.grey[500])));
+  }
 }
 
+/*
 class _HomeState extends State<Home> {
   List<String> _pageData;
 
@@ -31,7 +108,7 @@ class _HomeState extends State<Home> {
 
   @override
   void initState() {
-    _getListData(hasError: true).then((data) {
+    _getListData(hasError: false).then((data) {
       setState(() {
         if (data.length == 0) {
           data.add(
@@ -74,3 +151,4 @@ class _HomeState extends State<Home> {
     );
   }
 }
+*/
