577eb092e0e0712c89c99c268841d3f44ac90f92
Hakim Hauston <hakim.hauston@storms.com>
2021-08-23 23:14:05 +0800

    code: adding views and models
    -. https://www.filledstacks.com/post/flutter-architecture-scoped-model-implementation-guide/

---
+++ 007-scoped-model-guide/scoped_guide/lib/ui/views/home_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_guide/service_locator.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+import '../../scoped_model/home_model.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModel<HomeModel>(
+        model: locator<HomeModel>(),
+        child: ScopedModelDescendant<HomeModel>(
+          builder: (context, child, model) => Scaffold(
+              body: Center(
+            child: Text(model.title),
+          )),
+        ));
+  }
+}
