ec4e36ddb6445c8ea61b8210d088ff73b72c5d5a
Hakim Hauston <hakim.hauston@storms.com>
2021-08-31 15:00:43 +0800

    code: shared setup for all views
    -. https://www.filledstacks.com/post/flutter-architecture-my-provider-implementation-guide/#shared-setup-for-all-views

--- 010-provider-architecture/provider_start/lib/ui/views/login_view.dart
+++ 010-provider-architecture/provider_start/lib/ui/views/login_view.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:provider_architecture/core/enums/viewstate.dart';
 import 'package:provider_architecture/core/viewmodels/login_model.dart';
-import 'package:provider_architecture/locator.dart';
 import 'package:provider_architecture/ui/shared/app_colors.dart';
+import 'package:provider_architecture/ui/views/base_view.dart';
 import 'package:provider_architecture/ui/widgets/login_header.dart';
 
 class LoginView extends StatelessWidget {
@@ -11,35 +10,33 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<LoginModel>(
-      create: (context) => locator<LoginModel>(),
-      child: Consumer<LoginModel>(
-          builder: (context, model, child) => Scaffold(
-              backgroundColor: backgroundColor,
-              body: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    LoginHeader(
-                        validationMessage: model.errorMessage,
-                        controller: _controller),
-                    model.state == ViewState.Busy
-                        ? CircularProgressIndicator()
-                        : TextButton(
-                            child: Text(
-                              'Login',
-                              style: TextStyle(color: Colors.black),
-                            ),
-                            onPressed: () async {
-                              var loginSuccess =
-                                  await model.login(_controller.text);
-                              if (loginSuccess) {
-                                // Navigate to the home view
-                                Navigator.pushNamed(context, '/');
-                              }
-                            },
-                          )
-                  ]))),
-    );
+    // ignore: dead_code
+    return BaseView<LoginModel>(
+        builder: (context, model, child) => Scaffold(
+            backgroundColor: backgroundColor,
+            body: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  LoginHeader(
+                      validationMessage: model.errorMessage,
+                      controller: _controller),
+                  model.state == ViewState.Busy
+                      ? CircularProgressIndicator()
+                      : TextButton(
+                          child: Text(
+                            'Login',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          onPressed: () async {
+                            var loginSuccess =
+                                await model.login(_controller.text);
+                            if (loginSuccess) {
+                              // Navigate to the home view
+                              Navigator.pushNamed(context, '/');
+                            }
+                          },
+                        )
+                ])));
   }
 }
