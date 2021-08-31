eb898b04837698d85421a0d04b0dbf26d963a5e8
Hakim Hauston <hakim.hauston@storms.com>
2021-08-31 12:14:31 +0800

    code: attempt to fix dart http
    -. https://stackoverflow.com/questions/68992204/http-dart-invalid-argument-for-onerror-invalid-argument-onerror-error-handle

--- 010-provider-architecture/provider_start/lib/core/services/api.dart
+++ 010-provider-architecture/provider_start/lib/core/services/api.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:provider_architecture/core/models/comment.dart';
 import 'package:provider_architecture/core/models/post.dart';
@@ -13,10 +14,30 @@ class Api {
 
   Future<User> getUserProfile(int userId) async {
     // Get user profile for id
-    var response = await client.get('$endpoint/users/$userId');
+    try {
+      var response = await client.get('$endpoint/users/$userId').then((res) {
+        print('success');
+      }).catchError((Object err) {
+        print('fucker');
+        print(err);
+        throw err;
+      });
 
-    // Convert and return
-    return User.fromJson(json.decode(response.body));
+      // Convert and return
+      return User.fromJson(json.decode(response.body));
+    } on HttpException catch (ex) {
+      print('http');
+      print(ex);
+      print('exception');
+      return null;
+    } on Object catch (e) {
+      print('hello');
+      print(e);
+      print('world');
+      var szUser =
+          '{\r\n  \"id\": 5,\r\n  \"name\": \"Chelsey Dietrich\",\r\n  \"username\": \"Kamren\",\r\n  \"email\": \"Lucio_Hettinger@annie.ca\",\r\n  \"address\": {\r\n    \"street\": \"Skiles Walks\",\r\n    \"suite\": \"Suite 351\",\r\n    \"city\": \"Roscoeview\",\r\n    \"zipcode\": \"33263\",\r\n    \"geo\": {\r\n      \"lat\": \"-31.8129\",\r\n      \"lng\": \"62.5342\"\r\n    }\r\n  },\r\n  \"phone\": \"(254)954-1289\",\r\n  \"website\": \"demarco.info\",\r\n  \"company\": {\r\n    \"name\": \"Keebler LLC\",\r\n    \"catchPhrase\": \"User-centric fault-tolerant solution\",\r\n    \"bs\": \"revolutionize end-to-end systems\"\r\n  }\r\n}';
+      return User.fromJson(json.decode(szUser));
+    }
   }
 
   Future<List<Post>> getPostsForUser(int userId) async {
